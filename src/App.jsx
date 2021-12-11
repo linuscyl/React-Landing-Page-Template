e85448f0d0da262b6fd07e73d7297444c3b34551
linuscyl <linusyatlong@gmail.com>
2021-12-11 17:17:10 +0800

    Implement react-route-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { Navigation } from "./components/navigation";
 import "./App.css";
 import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Services } from "./components/services";
 
 const App = () => {
   return (
-    <div>
-      <Navigation />
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<Services />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
